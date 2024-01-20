e3c76e06d184cba3c737f3b5315e7e48fd64e1e1
Shakhzod Giyazov <shakhzodgiazov@gmail.com>
2024-01-20 23:13:20 +0300

    Add: gitignore, hidden secret keys

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -19,8 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+from decouple import config
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f%)ej+29ll1_7eodz91r_8wz7*$=z1j0^h#)07i40s_)m&rgb+'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
